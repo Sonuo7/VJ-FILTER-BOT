a942beac73884eb57eb3118cac1588984390f570
Sonuo7 <rockysingh12ops@gmail.com>
2024-07-20 12:15:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -71,8 +71,8 @@ CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "") # Necessary If clone 
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/koreans_dramass')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/hollywood_movies_in_hindidubs')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/wanda_updates')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/wanda_updates')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_To_Open_Linkl')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'hollywood_movies_in_hindidubs') # Support Chat Link Without https:// or @
 
